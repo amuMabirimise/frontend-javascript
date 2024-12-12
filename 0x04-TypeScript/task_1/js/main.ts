209de1bd8d77d9287fc41f39bd5713b9447fdecd
amuMabirimise <awmabirimise@gmail.com>
2024-12-12 09:13:29 +0200

    task 2  Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,17 +2,20 @@ interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
     location: string;
-    [propName: string]: any;
+    [propName: string]: any; 
+}
+  
+  interface Directors extends Teacher {
+    numberOfReports: number;
   }
   
-  const teacher3: Teacher = {
-    firstName: "John",
-    fullTimeEmployee: false,
-    lastName: "Doe",
-    location: "London",
-    contract: false,
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
   };
   
-  console.log(teacher3);
+  console.log(director1);
