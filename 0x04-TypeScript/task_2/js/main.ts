dbbfa7386cc3d34ea1e6c4b86ba6dd1832138e4c
amuMabirimise <awmabirimise@gmail.com>
2024-12-12 10:16:48 +0200

    task 6. Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,53 +1,63 @@
 
 interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
   }
-  
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
   }
-  
-  class Director implements DirectorInterface {
-    workFromHome(): string {
-      return "Working from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Getting a coffee break";
-    }
-  
-    workDirectorTasks(): string {
-      return "Getting to director tasks";
-    }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
   }
-  
-  class Teacher implements TeacherInterface {
-    workFromHome(): string {
-      return "Cannot work from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Cannot have a break";
-    }
-  
-    workTeacherTasks(): string {
-      return "Getting to work";
-    }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
   }
-  
-  function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === "number" && salary < 500) {
-      return new Teacher();
-    } else {
-      return new Director();
-    }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
   }
-  
-  console.log(createEmployee(200));
-  console.log(createEmployee(1000));
-  console.log(createEmployee('$500'));
-  
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+console.log(executeWork(createEmployee(200))); 
+console.log(executeWork(createEmployee(1000)));
